ee87c6dbc02af57a4a04696f17a39c1463c7e4f5
Pablo Calderon Bravo <pablocalderonbravo@gmail.com>
2023-04-11 20:14:17 -0600

    Adding the back delete function in the JS

--- 02-calculator/script.js
+++ 02-calculator/script.js
@@ -3,12 +3,12 @@ let operator = '';
 let value2 = '';
 const result = document.getElementById('result');
 
+let equal = false;
 
 
 // Add event listeners for all number buttons
 document.querySelectorAll('.NumberButton').forEach(function (button) {
     button.addEventListener('click', function () {
-        console.log(this.value);
       // If there's no operator, add the clicked digit to the first operand
       if (!operator) {
         value1 += this.value;
@@ -22,9 +22,35 @@ document.querySelectorAll('.NumberButton').forEach(function (button) {
   });
 
 
+  // CLEAR BUTTON
+const clearField = document.querySelector('.clear');
+clearField.addEventListener('click',
+    function () {
+        value1 = '';
+        value2 = '';
+        operator = '';
+        updateResult('');
+    }
+);
+
+// BACK DELETE
+const deleteNumber = document.querySelector('.back-delete');
+deleteNumber.addEventListener('click',
+  function () {
+      if (!equal && result.value){
+        if (!operator){
+          value1 = value1.slice(0,-1);
+          updateResult(value1);
+        } else {
+          value2 = value2.slice(0,-1);
+          updateResult(value2);
+        }
+      }
+  }
+);
+
 // Update the result element with a given value
 function updateResult(value) {
     result.value = value;
-    console.log(result.value);
   }
 
