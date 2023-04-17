ebe87fab8178ae9f36700f277bfc8c264192719f
Pablo Calderon Bravo <pablocalderonbravo@gmail.com>
2023-04-16 19:04:38 -0600

    All the functions and decimals are working properly, adding too the preview mode of the operation in curse

--- 02-calculator/script.js
+++ 02-calculator/script.js
@@ -2,15 +2,42 @@ let value1 = '';
 let operator = '';
 let value2 = '';
 const result = document.getElementById('result');
+let equalValidator = false;
+let equalValidatorButton = false;
+
+// Preview operation
+const value1Preview = document.getElementById('value1Display');
+const operatorPreview = document.getElementById('operatorDisplay');
+const value2Preview = document.getElementById('value2Display');
 
   // EQUAL ACTION
 const equal = document.querySelector('.equalButton');
-equal.addEventListener('click', function(){if (value2) {makeMathOperation()}});
+equal.addEventListener('click', function(){
+  if (value2) {
+    makeMathOperation();
+    equalValidatorButton = true;
+  }
+});
   
 
-// Add event listeners for all number buttons
+// NUMBERS
 document.querySelectorAll('.NumberButton').forEach(function (button) {
     button.addEventListener('click', function () {
+        if (equalValidator){
+          if (equalValidatorButton){
+            clearAll();
+            equalValidatorButton = false;
+            equalValidator = false;
+            updateResult(value1);
+          } else{
+            const tempNum = result.value;
+            const operatorTemp = operator;
+            clearAll();
+            value1 = tempNum;
+            operator = operatorTemp;
+            equalValidator = false;
+          }
+        }
         // If there's no operator, add the clicked digit to the first operand
         if (!operator) {
           value1 += this.value;
@@ -28,16 +55,30 @@ document.querySelectorAll('.NumberButton').forEach(function (button) {
 document.querySelectorAll('.MathOperations').forEach(function (button){
   button.addEventListener('click', function () {
     // if (result.value){
+    if (!equalValidator){  
       if (!operator){
         value1 = result.value;
         operator = this.value;
+        updateResult(value1);
       } else {
         if (value2) {
           makeMathOperation();
           operator = this.value;
+          updateResult(value1);
+        } else {
+          operator = this.value;
+          updateResult(value1);
         }
       }
     // }
+    } else {
+      const resultTemp = result.value;
+      clearAll();
+      value1 = resultTemp;
+      equalValidator = false;
+      operator = this.value;
+      updateResult(value1);
+    }
   });
 });
 
@@ -64,6 +105,7 @@ function makeMathOperation() {
         break;
       }
     }
+    equalValidator = true;
     clearAll();
     value1 = resultNum.toString();
     updateResult(value1);
@@ -73,7 +115,10 @@ function makeMathOperation() {
 
   // CLEAR BUTTON
 const clearField = document.querySelector('.clear');
-clearField.addEventListener('click', clearAll);
+clearField.addEventListener('click', function(){
+  equalValidator = false;
+  clearAll();
+});
 
 function clearAll() {
   value1 = '';
@@ -86,7 +131,8 @@ function clearAll() {
 const deleteNumber = document.querySelector('.back-delete');
 deleteNumber.addEventListener('click',
   function () {
-      if (result.value){
+      if (result.value && !equalValidator){
+        console.log("entra")
         if (!operator){
           value1 = value1.slice(0,-1);
           updateResult(value1);
@@ -98,10 +144,30 @@ deleteNumber.addEventListener('click',
   }
 );
 
-
-
-// Update the result element with a given value
+// UPDATE RESULT
 function updateResult(value) {
     result.value = value;
+    if (!equalValidator){
+      updatePreview();
+    }
   }
 
+
+// UPDATE PREVIEW
+function updatePreview(){
+  if (value1 && operator){
+    value1Preview.innerText = value1;
+    value1Preview.classList.remove('inactive');
+    operatorPreview.innerText = operator;
+    operatorPreview.classList.remove('inactive');
+  } else {
+    value1Preview.classList.add('inactive');
+    operatorPreview.classList.add('inactive');
+  }
+  if (value2){
+    value2Preview.innerText = value2;
+    value2Preview.classList.remove('inactive');
+  } else {
+    value2Preview.classList.add('inactive');
+  }
+}
